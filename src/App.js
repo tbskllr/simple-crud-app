f45aab80fcfa99bcab48c3c07fe14f7c834d4a1e
Tobias Keller <tbskllr@gmail.com>
2019-01-21 17:30:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editing: false };
+  }
+
+  edit = () => {
+    this.setState({ editing: true });
+  };
+
+  remove() {
+    alert("Removing comment");
+  }
+
+  save = () => {
+    var val = this.refs.newText.value;
+    console.log("New Comment: " + val);
+    this.setState({ editing: false });
+  };
+
+  renderForm() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="commentContainer">
+        <textarea ref="newText" defaultValue={this.props.children} />
+        <button onClick={this.save} className="button-save">
+          Save
+        </button>
       </div>
     );
   }
+
+  renderNormal() {
+    return (
+      <div className="commentContainer">
+        <div className="commentText">{this.props.children}</div>
+        <button onClick={this.edit} className="button-edit">
+          Edit
+        </button>
+        <button onClick={this.remove} className="button-remove">
+          Remove
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this.renderForm();
+    } else {
+      return this.renderNormal();
+    }
+  }
 }
 
-export default App;
+export default Comment;
